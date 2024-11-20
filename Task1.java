945f86ff102b8479bacb5da39a6edebc8c6d883f
bilal-qadir123 <bilalqadir2020@gmail.com>
2024-11-21 01:10:17 +0500

    Add files via upload

--- Task1.java
+++ Task1.java
@@ -1,22 +1,24 @@
 import java.lang.Math;
-public class Task1 extends Thread {
-	public void run(){
-		for(int i=0; i<=26; i++) {
-			int con=(int)(Math.random()*26);
-			char alpha=(char)('A'+con-1);
-			try
-			{
-			sleep(500); 
-			System.out.println(Thread.currentThread().getName());
-			}catch(InterruptedException e)
-			{
-				System.out.println(e);
-			}
-			if(alpha=='@') {
-				continue;
-			}
-			System.out.println(alpha);
-			}
 
-			}
-	}
+public class Main {
+    public static void main(String[] args) {
+        Task1 t1 = new Task1();
+        t1.start();
+        t1.interrupt();
+    }
+}
+
+class Task1 extends Thread {
+    public void run() {
+        for (int i = 0; i < 26; i++) {
+            int num = (int)(Math.random() * 26);
+            char alpha = (char)('A' + num);
+            try {
+                sleep(500);
+                System.out.println(Thread.currentThread().getName() + ": " + alpha);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
